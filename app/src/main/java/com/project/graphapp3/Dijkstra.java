1756ce280ca492f11f7566149ac7d410a985f45b
ViktoriiaKharchenko <ivanovad86@gmail.com>
2021-03-04 02:09:22 +0200

    finding shortest dist, dijkstra algorithm

---
+++ app/src/main/java/com/project/graphapp3/Dijkstra.java
@@ -0,0 +1,64 @@
+package com.project.graphapp3;
+
+import java.util.Stack;
+
+import static java.lang.Math.*;
+import static java.util.Arrays.*;
+
+public class Dijkstra {
+    int INF = Integer.MAX_VALUE / 2; // "Бесконечность"
+    int vNum; // количество вершин
+    int path[];
+    int[][] graph; // матрица смежности
+    public Dijkstra (int[][] graph, int vNum){
+        this.graph = new int[vNum][vNum];
+        for(int i =0; i<vNum; i++){
+            for (int j =0; j<vNum; j++){
+                if(graph[i][j] != 0){
+                    this.graph[i][j] = graph[i][j];
+                }
+                else this.graph[i][j] = INF;
+            }
+        }
+        this.vNum = vNum;
+
+    }
+    public int dijkstra(int start, int end) {
+        if(start == end){
+            path = new int[2];
+            path[0]=start; path[1] = end;
+            return 0;
+        }
+        boolean[] used = new boolean [vNum]; // массив пометок
+        int[] dist = new int [vNum];
+        int[] prev = new int [vNum];// массив расстояния. dist[v] = минимальное_расстояние(start, v)
+        fill(dist, INF); // устанаавливаем расстояние до всех вершин INF
+        fill(prev, -1);
+        dist[start-1] = 0; // для начальной вершины положим 0
+        for (;;) {
+            int v = -1;
+            for (int nv = 0; nv < vNum; nv++) // перебираем вершины
+                if (!used[nv] && dist[nv] < INF && (v == -1 || dist[v] > dist[nv])) // выбираем самую близкую непомеченную вершину
+                    v = nv;
+                if (v == -1) break; // ближайшая вершина не найдена
+            used[v] = true; // помечаем ее
+            for (int nv = 0; nv < vNum; nv++)
+                if (!used[nv] && graph[v][nv] < INF) { // для всех непомеченных смежных
+                    dist[nv] = min(dist[nv], dist[v] + graph[v][nv]);
+                    prev[nv] = v;
+                }
+        }
+        Stack stack = new Stack();
+        for (int v = end-1; v != -1; v = prev[v]) {
+            stack.push(v);
+        }
+        path = new int [stack.size()];
+        for (int i = 0; i < path.length; i++)
+            path[i] = (int) stack.pop() + 1 ;
+        if(dist[end-1] == INF){ path = null;return -1;}
+       return dist[end-1];
+    }
+    public int[] getShortestPath (){
+        return path;
+    }
+}
--- app/src/main/java/com/project/graphapp3/MatrixInput.java
+++ app/src/main/java/com/project/graphapp3/MatrixInput.java
@@ -1,6 +1,7 @@
 package com.project.graphapp3;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,12 +10,16 @@ import android.widget.TextView;
 import android.widget.LinearLayout;
 
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class MatrixInput extends AppCompatActivity {
     TextView textView;
+    TextView textViewDist;
+    TextView textViewShortPath;
     TextView nodesNumberView;
     EditText editMatrix;
     Button button;
@@ -24,6 +29,7 @@ public class MatrixInput extends AppCompatActivity {
     int state=0;
     int [][] prevMatrix;
     int [][] matrix;
+    int input1 =0, input2=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +43,12 @@ public class MatrixInput extends AppCompatActivity {
         infoContainer.setVisibility(View.INVISIBLE);
         node1 = findViewById(R.id.editTextNumberDecimal);
         node2 = findViewById(R.id.editTextNumberDecimal2);
+        textViewDist = findViewById(R.id.dist);
+        textViewShortPath= findViewById(R.id.path);
+
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public void onClick(View view) {
         if(state == 0) {
             String errorMessage = checkMatrix();
@@ -54,7 +64,17 @@ public class MatrixInput extends AppCompatActivity {
                 errorMessage=checkNodes();
             if (errorMessage == null){
                 textView.setText("");
-
+                Dijkstra alg = new Dijkstra(matrix, matrix.length);
+                int dist = alg.dijkstra(input1,input2);
+                if(dist!=-1){
+                textViewDist.setText("Shortest distance = "+dist);
+                int [] path = alg.getShortestPath();
+                String pathText = "Shortest path : "+path[0];
+                for(int i =1; i<path.length; i++){
+                    pathText += (" -> "+path[i]);
+                }
+                textViewShortPath.setText(pathText);
+                }else textViewDist.setText("There is no path");
             }
             else {
                 textView.setText(errorMessage);
@@ -76,10 +96,11 @@ public class MatrixInput extends AppCompatActivity {
     private void ErrorCheck(String errorMessage){
         if (errorMessage == null) {
             textView.setText("");
-            int numOfNodes = matrix.length;
             prevMatrix = matrix;
             state=1;
             nodesNumberView.setText("number of nodes : " + matrix.length);
+            node1.setText("");
+            node2.setText("");
             infoContainer.setVisibility(View.VISIBLE);
         } else {
             textView.setText(errorMessage);
@@ -88,6 +109,8 @@ public class MatrixInput extends AppCompatActivity {
     }
     private String checkMatrix() {
         String errorMessage = null;
+        textViewDist.setText("");
+        textViewShortPath.setText("");
         String rawMatrixString = editMatrix.getText().toString().trim().replaceAll("[ ]","");
         if(rawMatrixString.contains(",,")){
             errorMessage = "skiped number betwin ',' and ','";
@@ -105,7 +128,7 @@ public class MatrixInput extends AppCompatActivity {
         for (int i = 0; i < text.length; i++) {
             String[]matrixRow=text[i].split(",");
             if (matrixRow.length != width) {
-                errorMessage = "wrond matrix format! row [" + i + "] length:" + matrixRow.length + " but must be : " + width;
+                errorMessage = "wrong matrix format! row [" + i + "] length:" + matrixRow.length + " but must be : " + width;
                 matrix=null;
                 return errorMessage;
             }
@@ -131,20 +154,20 @@ public class MatrixInput extends AppCompatActivity {
 
     private String checkNodes() {
         String errorMessage = null;
-        int input1 =0, input2=0;
+        textViewDist.setText("");
+        textViewShortPath.setText("");
         try{
         input1 = Integer.parseInt(node1.getText().toString());
         input2 = Integer.parseInt(node2.getText().toString());
         }catch (Exception e){
             errorMessage = "Empty nodes. Enter the nodes for finding the path";
             return  errorMessage;
-
         }
-
         if(input1 <1 || input1 > matrix.length || input2 <1 || input2 > matrix.length){
             errorMessage = "Not existing node";
             return errorMessage;
         }
+
         return errorMessage;
     }
     @Override
